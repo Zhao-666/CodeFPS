ad217810a1cd6493cc6f735a2b7c5b9acbe2b46f
Max <2268699370@qq.com>
2021-11-05 23:48:14 +0800

    优化爬梯效果

--- Scripts/Game/Buildings/LadderController.cs
+++ Scripts/Game/Buildings/LadderController.cs
@@ -6,17 +6,4 @@ public class LadderController : MonoBehaviour
     [Header("Ladder Trigger"), SerializeField]
     private BoxCollider ladderTrigger;
 
-    private void OnCollisionEnter(Collision other)
-    {
-        var center = ladderTrigger.center;
-        Vector3 pos = new Vector3(center.x, center.y + 0.5f, center.z - 0.2f);
-        ladderTrigger.center = pos;
-    }
-
-    private void OnCollisionExit(Collision other1)
-    {
-        var center = ladderTrigger.center;
-        Vector3 pos = new Vector3(center.x, center.y - 0.5f, center.z + 0.2f);
-        ladderTrigger.center = pos;
-    }
 }
