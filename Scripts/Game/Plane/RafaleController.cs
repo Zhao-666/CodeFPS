b3d17c9cf982a4d585ca47316843426e33afbfd9
Max <2268699370@qq.com>
2021-11-30 12:07:15 +0800

    增加可飞行飞机预制体

---
+++ Scripts/Game/Plane/RafaleController.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+public class RafaleController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * 0.3f,Space.Self);
+    }
+}
